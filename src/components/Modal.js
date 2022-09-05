d940bb03c742e8585ba2ad8de5f6e9590762c03a
Nick Melms <nmelms92@gmail.com>
2022-09-05 16:39:54 -0400

    added styling on modal also added btn background and hover color

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,26 +6,28 @@ export default function Modal() {
   return (
     <div className="modal">
       <div className="closeBtn">X</div>
-      <div className="modalTitle">
+      <div className="all">
         <FontAwesomeIcon className="tagIcon" size="4x" icon={faTags} />
-        <div className="titleText">
-          <h2>Before You Go....</h2>
-          <h2>Would 50% off 6months Help?</h2>
-        </div>
-        <body>
-          <p>
-            We know things REALLY suck in the world right now - and many
-            business and freelancers are struggling to stay afloat during the
-            COVID-19 crisis.
-          </p>
-          <p>
-            So if you could use some extra cushion, grab 50% off for 6 months.
-          </p>
-          <p>We hope youre staying safe and Healthy</p>
-        </body>
-        <div>
-          <button>50% off for 6 monthhs</button>
-          <button>No thanks, ill cancel</button>
+        <div className="modalTitle">
+          <div className="titleText">
+            <h2>Before You Go....</h2>
+            <h2>Would 50% off 6months Help?</h2>
+          </div>
+          <body className="modalBody">
+            <p>
+              We know things REALLY suck in the world right now - and many
+              business and freelancers are struggling to stay afloat during the
+              COVID-19 crisis.
+            </p>
+            <p className="modalBoldText">
+              So if you could use some extra cushion, grab 50% off for 6 months.
+            </p>
+            <p>We hope youre staying safe and Healthy</p>
+          </body>
+          <div className="modalButtons">
+            <button className="primaryBtn btn">50% off for 6 monthhs</button>
+            <button className="cancelBtn btn">No thanks, ill cancel</button>
+          </div>
         </div>
       </div>
     </div>
