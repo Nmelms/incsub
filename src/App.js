7e4530b85cf401d23f21a704ec9a13dc78136ce1
Nick Melms <nmelms92@gmail.com>
2022-09-05 14:48:07 -0400

    created a modal component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Modal from "./components/Modal";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className></div>
+      <h2>My Account</h2>
+      <Modal />
+    </div>
+  );
 }
 
 export default App;
