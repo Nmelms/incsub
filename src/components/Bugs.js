954c6277a7efcf0cab27ccd9a47a7d1b9857c0e6
Nick Melms <nmelms92@gmail.com>
2022-09-08 00:26:45 -0400

    drop down displays over div below, check boxes stay check or unchecked even after opening and closing dropdown

--- src/components/Bugs.js
+++ src/components/Bugs.js
@@ -25,29 +25,58 @@ export default function Bugs() {
   };
 
   return (
-    <div className="bugs">
-      <p>What product(s) did you have an issue with?</p>
-      <div onClick={() => handleClick()} className="dropDownInput">
-        <p>select Produt(s)</p>
-        {checkedItems.length === 1 && `${checkedItems.length} product Selected`}
-        {checkedItems.length > 1 && `${checkedItems.length} products Selected`}
-      </div>
-      {showDropDown && (
-        <div className="dropDownContent">
-          <label onChange={(e) => handleCheck(e)} htmlFor="product1">
-            <input value="product1" id="product1" type="checkbox" />
-            product1
-          </label>
-          <label onChange={(e) => handleCheck(e)} htmlFor="product2">
-            <input value="product2" id="product2" type="checkbox" />
-            product2
-          </label>
-          <label onChange={(e) => handleCheck(e)} htmlFor="product3">
-            <input value="product3" id="product3" type="checkbox" />
-            product3
-          </label>
+    <>
+      <div className="bugs">
+        <p>What product(s) did you have an issue with?</p>
+
+        <div onClick={() => handleClick()} className="dropDownInput">
+          <p>select Produt(s)</p>
+          {checkedItems.length === 1 &&
+            `${checkedItems.length} product Selected`}
+          {checkedItems.length > 1 &&
+            `${checkedItems.length} products Selected`}
         </div>
-      )}
-    </div>
+        {showDropDown && (
+          <div className="dropDownContent">
+            <label onChange={(e) => handleCheck(e)} htmlFor="product1">
+              <input
+                checked={checkedItems.includes("product1") ? true : false}
+                value="product1"
+                id="product1"
+                type="checkbox"
+              />
+              product1
+            </label>
+            <label onChange={(e) => handleCheck(e)} htmlFor="product2">
+              <input
+                checked={checkedItems.includes("product2") ? true : false}
+                value="product2"
+                id="product2"
+                type="checkbox"
+              />
+              product2
+            </label>
+            <label onChange={(e) => handleCheck(e)} htmlFor="product3">
+              <input
+                checked={checkedItems.includes("product3") ? true : false}
+                value="product3"
+                id="product3"
+                type="checkbox"
+              />
+              product3
+            </label>
+          </div>
+        )}
+        <div className="toggle">
+          <p>What was it?</p>
+          <button>one</button>
+          <button>two</button>
+        </div>
+        <div>
+          <p>What problem(s) did you encounter</p>
+          <textarea></textarea>
+        </div>
+      </div>
+    </>
   );
 }
