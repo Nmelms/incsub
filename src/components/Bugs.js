215f7584f94e4d8af12420f48cd5cf3b261392d6
Nick Melms <nmelms92@gmail.com>
2022-09-09 17:11:27 -0400

    finshed input animation when active

--- src/components/Bugs.js
+++ src/components/Bugs.js
@@ -33,65 +33,71 @@ export default function Bugs() {
     <>
       <div className="bugs">
         <p>What product(s) did you have an issue with?</p>
-
-        <div
-          ref={inputRef}
-          onClick={() => handleClick()}
-          className="dropDownInput"
-        >
-          <p>select Produt(s)</p>
-          <div className="productNumber">
-            {checkedItems.length === 1 &&
-              `${checkedItems.length} product Selected`}
-            {checkedItems.length > 1 &&
-              `${checkedItems.length} products Selected`}
+        <div className="productDiv">
+          <div
+            ref={inputRef}
+            onClick={() => handleClick()}
+            className="dropDownInput"
+          >
+            <p>select Produt(s)</p>
+            <div className="productNumber">
+              {checkedItems.length === 1 &&
+                `${checkedItems.length} product Selected`}
+              {checkedItems.length > 1 &&
+                `${checkedItems.length} products Selected`}
+            </div>
           </div>
+          {showDropDown && (
+            <div className="dropDownContent">
+              <label htmlFor="product1">
+                <input
+                  onChange={(e) => handleCheck(e)}
+                  checked={checkedItems.includes("product1") ? true : false}
+                  value="product1"
+                  id="product1"
+                  type="checkbox"
+                />
+                product1
+              </label>
+              <label htmlFor="product2">
+                <input
+                  onChange={(e) => handleCheck(e)}
+                  checked={checkedItems.includes("product2") ? true : false}
+                  value="product2"
+                  id="product2"
+                  type="checkbox"
+                />
+                product2
+              </label>
+              <label htmlFor="product3">
+                <input
+                  onChange={(e) => handleCheck(e)}
+                  checked={checkedItems.includes("product3") ? true : false}
+                  value="product3"
+                  id="product3"
+                  type="checkbox"
+                />
+                product3
+              </label>
+            </div>
+          )}
         </div>
-        {showDropDown && (
-          <div className="dropDownContent">
-            <label htmlFor="product1">
-              <input
-                onChange={(e) => handleCheck(e)}
-                checked={checkedItems.includes("product1") ? true : false}
-                value="product1"
-                id="product1"
-                type="checkbox"
-              />
-              product1
-            </label>
-            <label htmlFor="product2">
-              <input
-                onChange={(e) => handleCheck(e)}
-                checked={checkedItems.includes("product2") ? true : false}
-                value="product2"
-                id="product2"
-                type="checkbox"
-              />
-              product2
-            </label>
-            <label htmlFor="product3">
-              <input
-                onChange={(e) => handleCheck(e)}
-                checked={checkedItems.includes("product3") ? true : false}
-                value="product3"
-                id="product3"
-                type="checkbox"
-              />
-              product3
-            </label>
-          </div>
-        )}
+
         <div className="toggle">
           <p>What was it?</p>
           <div className="problemBtns">
-            <label htmlFor="one">
-              <input type="radio" id="one" name="problems" />
-              <span>One Problem</span>
-            </label>
-            <label htmlFor="various">
-              <input type="radio" id="various" name="problems" />
-              <span>various problems</span>
-            </label>
+            <div>
+              <label className="btnLabel" htmlFor="one">
+                <input type="radio" id="one" name="problems" />
+                <span>One Problem</span>
+              </label>
+            </div>
+            <div>
+              <label htmlFor="various">
+                <input type="radio" id="various" name="problems" />
+                <span>various problems</span>
+              </label>
+            </div>
           </div>
         </div>
         <div className="problemText">
